769fe1af862e4758604d8a3bc183bbd5998c2f48
0x414A43 <andrew.j.cabrera@gmail.com>
2023-01-28 22:29:36 +1030

    commit

--- peh/readwrite.py
+++ peh/readwrite.py
@@ -1,4 +1,23 @@
 #!/bin/python3
 
+#days = open('days.txt', "w") #write
+days = open('days.txt', "a") #append
+#days.write("Monday")
+days.write("\nTuesday")
+           
+days.close()
+
+"""
 months = open('months.txt')
-print(Months)
+
+# print(months.read())
+
+
+for month in months:
+    print(month.strip())
+
+
+
+#months.close()
+
+"""
